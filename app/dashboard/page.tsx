2b20834dd11c2164149f9bcf9c17b7b9075ad2f2
Samson Ocran <samson.ocran@gmail.com>
2024-01-22 20:28:00 +0100

    prev commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import BarChart from "@/components/barchart/BarChart";
+import Button from "@/components/button/Button";
 import CareersCard from "@/components/careers/CareersCard";
 import MainLayout from "@/components/main-layout/MainLayout";
 import ViewersCard from "@/components/viewers-card/ViewersCard";
@@ -38,40 +41,58 @@ const Dashboard = () => {
             rate="(100%)"
           />
         </div>
-        <div className="flex justify-between my-6">
-          <div className="w-[74%]">
+        <div className="w-[100%] flex flex-col lg:flex-row lg:gap-11 my-6">
+          <div className="w-[100%] lg:w-[74%]">
             <BarChart />
           </div>
-          <div className="w-[20]">
+          <div className="w-[100%] my-5 lg:my-0 lg:w-[20%] lg:mx-0">
             <ViewersCard>
               <h3 className="font-medium text-base pb-3 px-2">Viwers</h3>
               <div className="flex justify-between items-center px-2">
                 <div>
                   <div className="flex flex-col text-[10px]">
                     <span className="flex items-center pb-1">
-                      <span className="rounded-full text-pink-100 bg-pink-100 w-[6px] h-[6px] mr-2">.</span>
+                      <span className="rounded-full text-pink-100 bg-pink-100 w-[6px] h-[6px] mr-2">
+                        .
+                      </span>
                       <span>8</span>
                     </span>
-                    <span  className="text-lightgrey-400">Incognito</span>
+                    <span className="text-lightgrey-400">Incognito</span>
                   </div>
                   <div className="flex flex-col text-[10px] my-4">
                     <span className="flex items-center pb-1">
-                      <span className="rounded-full text-blue-100 bg-blue-100 w-[6px] h-[6px] mr-2">.</span>
+                      <span className="rounded-full text-blue-100 bg-blue-100 w-[6px] h-[6px] mr-2">
+                        .
+                      </span>
                       <span className="text-lightgrey-400">5</span>
                     </span>
-                    <span  className="text-lightgrey-400">User</span>
+                    <span className="text-lightgrey-400">User</span>
                   </div>
                   <div className="flex flex-col text-[10px]">
                     <span className="flex items-center pb-1">
-                      <span className="rounded-full text-blue-250 bg-blue-250 w-[6px] h-[6px] mr-2">.</span>
+                      <span className="rounded-full text-blue-250 bg-blue-250 w-[6px] h-[6px] mr-2">
+                        .
+                      </span>
                       <span className="text-lightgrey-400">10</span>
                     </span>
-                    <span  className="text-lightgrey-400">Recruiters</span>
+                    <span className="text-lightgrey-400">Recruiters</span>
                   </div>
                 </div>
                 <div>
                   <Image src="/pie.png" width={100} height={100} alt="pie" />
                 </div>
+              </div>
+            </ViewersCard>
+            <ViewersCard>
+              <div>
+                <input type="email" placeholder="E-mail" className="py-2.5 px-3 mt-2 md:py-3.5 lg:py-2.5 rounded-md bg-lightgrey-100" />
+                <Button
+                  disable={false}
+                  onClick={(e) => console.log("Subscribing...", e)}
+                  additionalStyles="flex justify-between items-center text-sm py-2 rounded w-[30%] px-3 text-white-100"
+                >
+                  <span>Subscribe</span>
+                </Button>
               </div>
             </ViewersCard>
           </div>
--- components/viewers-card/ViewersCard.tsx
+++ components/viewers-card/ViewersCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ViewersCard: React.FC<VeiwersCardProps> = ({children}) => {
   return (
-    <div className='p-4 bg-white-100 rounded-2xl w-[250px] shadow-cardShadow'>
+    <div className='p-4 bg-white-100 rounded-2xl w-[80%] md:w-[60%] lg:w-[250px] mx-auto shadow-cardShadow'>
         {children}
     </div>
   )
